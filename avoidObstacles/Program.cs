bd7f04fb6483b5f74986bd24e39579b4f21ad90d
Hayk <hayk.hayr3@gmail.com>
2020-09-03 23:50:25 +0400

    Add project files.

---
+++ avoidObstacles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace avoidObstacles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array length: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] inputArray = new int[length];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.Write("Enter an element: ");
+                inputArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(avoidObstacles(inputArray));
+            Console.ReadLine();
+        }
+        static int avoidObstacles(int[] inputArray)
+        {
+            int num = 2;
+            bool isRight = false;
+            while (isRight == false)
+            {
+                isRight = true;
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    if (inputArray[i] % num == 0)
+                    {
+                        num++;
+                        isRight = false;
+                        break;
+                    }
+                }
+            }
+            return num;
+        }
+    }
+}
